7885a2ba94ea711ff9e0435e7089a32ccdce78f1
Sagarika Mohanty <sagarikamohanty.official@gmail.com>
2024-01-05 12:03:21 +0530

    components/split/multipleComponents

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,14 @@
+
+import "./ExpenseDate.css";
+
+function ExpenseDate(data) {
+    const day = data.date.toLocaleString('en-Us', {month : 'long'});
+    const month = data.date.toLocaleString('en-Us', {day : '2-digit'});
+    const year = data.date.getFullYear();
+
+    return (
+     <div className = "expense_Date">{day}{" "}{month}{" "}{year}{"             "}</div>
+    );
+}
+
+export default ExpenseDate;
